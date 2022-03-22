da8501d0ce78bb0aa60bebafc557ef7f985a3951
Paulo C. Silva Jr <pauluscave@gmail.com>
2022-03-22 17:42:38 -0300

    Adicionado em main testes para criar banco e tabelas e inserir dados

--- src/main.py
+++ src/main.py
@@ -1,6 +1,11 @@
 import sys
+# import time
+
 from separador import Separador
 from validar_doc import ValidarDOC
+from criar_bd_tabela import criar_db_tabela
+from inserir_registros import inserir_registros
+
 
 def main():
     arquivo = sys.argv[1]
@@ -11,14 +16,20 @@ def main():
     sep = Separador(texto)
     dados = sep.retornar_elementos_em_lista()
 
-    print(dados[0])
+    print('Colunas em arquivo base:', dados[0], sep='\n', end='\n\n')
 
     valcpf = ValidarDOC(dados[1:])
     lista_doc_invalido = valcpf.validar()
     if not lista_doc_invalido:
         print('NÃO hã CPF/CNPJ inválidos')
     else:
-        print('Itens com CPF/CNPJ inválido', *lista_doc_invalido, sep='\n')
+        print('Itens com CPF/CNPJ inválido', *lista_doc_invalido, sep='\n', end='\n\n')
+
+    criar_db_tabela()
+    dados_parciais = dados[1:]
+    inserir_registros(dados_parciais)
+
+    # time.sleep(600)
 
 
 if __name__ == '__main__':
